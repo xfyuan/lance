9a56d7fa52c988c94acc1ad1227d5bff15774c5f
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-07-18 01:28:02 +0800

    add article resource

--- app/models/friend.js
+++ app/models/friend.js
@@ -1,11 +1,13 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  firstName:     DS.attr('string'),
-  lastName:      DS.attr('string'),
-  email:         DS.attr('string'),
-  twitter:       DS.attr('string'),
-  totalArticles: DS.attr('number'),
+  articles      : DS.hasMany('article'),
+  email         : DS.attr('string'),
+  firstName     : DS.attr('string'),
+  lastName      : DS.attr('string'),
+  totalArticles : DS.attr('number'),
+  twitter       : DS.attr('string'),
+
   fullName: Ember.computed('firstName', 'lastName', function(){
     return this.get('firstName') +' '+ this.get('lastName');
   })
