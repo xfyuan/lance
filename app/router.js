bcefe2ccc611d39dfe675ef3912401e99109ff33
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-07-18 21:26:48 +0800

    implement new articles

--- app/router.js
+++ app/router.js
@@ -10,6 +10,7 @@ Router.map(function() {
     this.route('new');
     this.route('show', { path: ':friend_id' }, function() {
       this.resource('articles', function() {
+        this.route('new');
       });
     });
     this.route('edit', { path: ':friend_id/edit' });
