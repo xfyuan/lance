9c68067cc9cb82eb15d56219bae093520e2330e3
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-07-18 23:04:11 +0800

    implement autosave article when changing its state

--- app/components/articles/article-row.js
+++ app/components/articles/article-row.js
@@ -4,9 +4,16 @@ export default Ember.Component.extend({
   tagName: 'tr',
   article: null,
   articleStates: null,
-  actions: {
-    saveArticle: function(article) {
+  autoSave: function() {
+    var article = this.get('article');
+    if (!article.get('isNew')) {
       this.sendAction('save', article);
     }
-  }
+  },
+  stateChanged: Ember.observer('article.state', function() {
+    var article = this.get('article');
+    if (article.get('hasDirtyAttributes') && !article.get('isSaving')) {
+      Ember.run.once(this, this.autoSave);
+    }
+  }).on('init')
 });
