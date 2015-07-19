9de4cd964f9bcbb619edb85477f9fe006858a2a1
Xiaofeng Yuan <xfyuan@thoughtworks.com>
2015-07-19 12:43:31 +0800

    switch to V4 api to get data by using 'links'

--- app/routes/articles/index.js
+++ app/routes/articles/index.js
@@ -2,7 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
-    return this.modelFor('friends/show').get('articles');
+    var articles = this.modelFor('friends/show').get('articles');
+
+    if (articles.get('isFulfilled')) {
+      articles.reload();
+    }
+    return articles;
   },
   actions: {
     save: function(model) {
